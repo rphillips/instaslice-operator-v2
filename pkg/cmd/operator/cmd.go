b82e1d181dff20b8307546dbb710d84505651215
Ryan Phillips <rphillips@redhat.com>
2025-02-27 13:28:51 -0600

    add version and daemonset shim

--- pkg/cmd/operator/cmd.go
+++ pkg/cmd/operator/cmd.go
@@ -4,16 +4,17 @@ import (
 	"context"
 
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/version"
+
 	"k8s.io/utils/clock"
 
 	"github.com/openshift/instaslice-operator/pkg/operator"
+	"github.com/openshift/instaslice-operator/pkg/version"
 	"github.com/openshift/library-go/pkg/controller/controllercmd"
 )
 
 func NewOperator(ctx context.Context) *cobra.Command {
 	cmd := controllercmd.
-		NewControllerCommandConfig("instaslice-operator", version.Info{}, operator.RunOperator, clock.RealClock{}).
+		NewControllerCommandConfig("instaslice-operator", version.Get(), operator.RunOperator, clock.RealClock{}).
 		NewCommandWithContext(ctx)
 	cmd.Use = "operator"
 	cmd.Short = "Start the Cluster Instaslice Operator"
